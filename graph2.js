bb8ef5210668081f7d992a56eb7e91255afffaaa
mvb43 <33532536+mvb43@users.noreply.github.com>
2021-03-29 23:05:31 -0400

    fixed code appearance

--- graph2.js
+++ graph2.js
@@ -9,19 +9,29 @@ document.addEventListener('DOMContentLoaded', _ => {
     "bottom": 100,
     "left": 150
   };
+
   const chartWidthH = widthH - marginsH.left - marginsH.right;
   const chartHeightH = heightH - marginsH.top - marginsH.bottom;
+
+  //create g tags
   let annotations = svghealth.append("g");
   let chart = svghealth.append("g").attr("transform", "translate(" + marginsH.left + "," + marginsH.top + ")");
+
+  //begin data enter
   d3.json("./health_factors.json", d3.autoType).then((data) => {
+    //create subgroups for split scales
     let groups = Array.from(new Set(Object.values(data).map(d => d.risk_factor)));
     let subgroups = Array.from(new Set(Object.values(data).map(d => d.gender))).slice(0, 2);
+
+    //create scales
     let xScaleH = d3.scaleLinear().domain([0, 20]).range([0, chartWidthH]);
     let yScaleH = d3.scaleBand().domain(groups).range([chartHeightH, 0]).padding([0.05]);
     let ySubgroup = d3.scaleBand()
       .domain(subgroups)
       .range([0, yScaleH.bandwidth()])
       .padding([0.03]);
+
+    //create axes
     let bottomAxisH = d3.axisBottom(xScaleH);
     let bottomGridLinesH = d3.axisBottom(xScaleH).tickFormat("").tickSize(-chartHeightH);
     annotations.append("g").attr("class", "x axis")
@@ -31,11 +41,14 @@ document.addEventListener('DOMContentLoaded', _ => {
     annotations.append("g").attr("class", "gridlines")
       .attr("transform", "translate(" + (marginsH.left) + "," + (marginsH.top + chartHeightH) + ")")
       .call(bottomGridLinesH);
+
     let leftAxisH = d3.axisLeft(yScaleH).tickFormat(tick => tick.charAt(0).toUpperCase() + tick.slice(1).replace(/_/g, " "));
     annotations.append("g").attr("class", "y axis")
       .attr("transform", "translate(" + (marginsH.left) + "," + (marginsH.top) + ")")
       .raise()
       .call(leftAxisH);
+
+    //begin data join, separated for text and rects
     const groupEnter = chart.selectAll("g").data(groups)
       .enter()
       .append("g")
@@ -43,6 +56,7 @@ document.addEventListener('DOMContentLoaded', _ => {
       .selectAll("rect")
       .data(d => Object.values(data).filter(e => e.risk_factor === d))
       .enter()
+
     groupEnter.append("rect")
       .lower()
       .attr("x", xScaleH(0))
@@ -57,21 +71,27 @@ document.addEventListener('DOMContentLoaded', _ => {
           return d3.interpolateReds(0.75);
         }
       })
+
+    //Append text on graph
     groupEnter.append("text")
       .text(d => d.percentage.toFixed(2) + "%")
       .attr("x", 10)
       .attr("y", d => ySubgroup(d.gender) + ySubgroup.bandwidth() / 2 + 7)
       .attr("fill", "white")
+
     svghealth.append("text")
       .text("Health Factors")
       .attr("transform", `rotate(270,50,${heightH/2})`)
       .attr("text-anchor", "middle")
       .attr("x", 50)
       .attr("y", heightH / 2)
+
     svghealth.append("text")
       .text("Percentage of Strokes")
       .attr("x", widthH / 2)
       .attr("y", 700)
+
+    //Append legend
     svghealth.append("circle").attr("cx", 900).attr("cy", 80).attr("r", 8).style("fill", d3.interpolateBlues(0.8))
     svghealth.append("circle").attr("cx", 900).attr("cy", 110).attr("r", 8).style("fill", d3.interpolateReds(0.75))
     svghealth.append("text").attr("x", 920).attr("y", 85).text("Male").style("font-size", "15px").attr("alignment-baseline", "middle")
